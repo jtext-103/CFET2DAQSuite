966565e5c95ee57bfb01044aa8f846b1049a635d
wangyuxing <wyxchina@foxmail.com>
2018-12-12 17:09:49 +0800

    初始化App

---
+++ Code/CFET2App/Cfet2ProgramAddThingsPartial.cs
@@ -0,0 +1,19 @@
+﻿using Jtext103.CFET2.CFET2App.ExampleThings;
+using Jtext103.CFET2.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jtext103.CFET2.CFET2App
+{
+    partial  class Cfet2Program : CFET2Host
+    {
+        private void AddThings()
+        {
+            PcMonitorThing pc = new PcMonitorThing();
+            MyHub.TryAddThing(pc, "/", "pc");
+        }
+    }
+}
