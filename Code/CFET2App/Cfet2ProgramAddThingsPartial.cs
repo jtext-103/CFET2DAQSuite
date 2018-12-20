e39dd509875c751048ccb99627fc32b883b9e3da
wangyuxing <wyxchina@foxmail.com>
2018-12-20 10:45:44 +0800

    修改到能用，从卡能随动

--- Code/CFET2App/Cfet2ProgramAddThingsPartial.cs
+++ Code/CFET2App/Cfet2ProgramAddThingsPartial.cs
@@ -47,24 +47,28 @@ namespace Jtext103.CFET2.CFET2App
             MyHub.TryAddThing(niAlone,      //上面的niAlone
                                 @"/",       //Thing挂载路径，都一样，不要改！！！
                                 "Card0",    //卡名，也就是在网页上看到的卡名称
-                                //下面引号中的要改，前面的是配置文件路径，后面的是采集数据保存到本地的路径
+                                            //下面引号中的要改，前面的是配置文件路径，后面的是采集数据保存到本地的路径
                                 new { ConfigFilePath = @"D:\Run\ConfigFile\DAQFamilyBucket\niAlone.txt", DataFileParentDirectory = @"D:\Data\ni\Card0" });
 
-            //var niMaster = new AIThing();
-            //niMaster.basicAI = new NIAI();
-            //niMaster.DataFileFactory = new HDF5DataFileFactory();
-            //MyHub.TryAddThing(niMaster,      
-            //                    @"/",       
-            //                    "Card1",    
-            //                    new { ConfigFilePath = @"D:\Run\ConfigFile\DAQFamilyBucket\niMaster.txt", DataFileParentDirectory = @"D:\Data\ni\Card1" });
+            //注意，这里加了多少个卡，在左边
+            //解决方案资源管理器 -> CFET2App -> Views -> ViewSelector.json 中的 childpath 字段中
+            //就要加对应多少个卡，且名字和卡名要一样，否则网页上不能显示
 
-            //var niSlave = new AIThing();
-            //niSlave.basicAI = new NIAI();
-            //niSlave.DataFileFactory = new HDF5DataFileFactory();
-            //MyHub.TryAddThing(niSlave,
-            //                    @"/",
-            //                    "Card2",
-            //                    new { ConfigFilePath = @"D:\Run\ConfigFile\DAQFamilyBucket\niSlave.txt", DataFileParentDirectory = @"D:\Data\ni\Card2" });
+            var niMaster = new AIThing();
+            niMaster.basicAI = new NIAI();
+            niMaster.DataFileFactory = new HDF5DataFileFactory();
+            MyHub.TryAddThing(niMaster,
+                                @"/",
+                                "Card1",
+                                new { ConfigFilePath = @"D:\Run\ConfigFile\DAQFamilyBucket\niMaster.txt", DataFileParentDirectory = @"D:\Data\ni\Card1" });
+
+            var niSlave = new AIThing();
+            niSlave.basicAI = new NIAI();
+            niSlave.DataFileFactory = new HDF5DataFileFactory();
+            MyHub.TryAddThing(niSlave,
+                                @"/",
+                                "Card2",
+                                new { ConfigFilePath = @"D:\Run\ConfigFile\DAQFamilyBucket\niSlave.txt", DataFileParentDirectory = @"D:\Data\ni\Card2" });
 
             //------------------------------采集卡管理，只有一个这个
             var aiManagement = new AIManagementThing();
@@ -73,10 +77,10 @@ namespace Jtext103.CFET2.CFET2App
                                 "aimanagement",
                                 new
                                 {
-                                    //引号里面有几个就加几个，注意前面是/后面是卡名，比如{ "/Card0", "/Card1" },
-                                    AllAIThingPaths = new string[] { "/Card0" },
+                                    //有多少个卡就加几个，注意前面是 / 后面是卡名，比如{ "/Card0", "/Card1" },
+                                    AllAIThingPaths = new string[] { "/Card0", "/Card1", "/Card2" },
                                     //自动Arm的，如果不想手动触发的就加上，跟上面一行格式一样
-                                    AutoArmAIThingPaths = new string[] {  }
+                                    AutoArmAIThingPaths = new string[] { "/Card2" }
                                 });
 
             //------------------------------上传文件的，只有一个这个
