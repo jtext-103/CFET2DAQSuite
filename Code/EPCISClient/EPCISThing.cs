8af61263e7826ab3873ab89e49ca76569efb316e
wangyuxing <wyxchina@foxmail.com>
2019-06-05 20:16:05 +0800

    修改EPCISThing，增加GetPVWithDoubleCheck方法

--- Code/EPCISClient/EPCISThing.cs
+++ Code/EPCISClient/EPCISThing.cs
@@ -36,6 +36,28 @@ namespace Jtext103.CFET2.Things.EPCISClient
         }
 
         /// <summary>
+        /// 连续查询3次PV值，如果全相同则返回，否则重复此步骤2次，直到值相同或返回Nothing
+        /// 用于实时性低但需要审慎操作的情况，防止EPCIS抽风
+        /// </summary>
+        /// <param name="pvName"></param>
+        /// <returns></returns>
+        [Cfet2Status]
+        public object GetPVWithDoubleCheck(string pvName)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                var firstGet = epciser.TryGetPV(pvName);
+                var secondGet = epciser.TryGetPV(pvName);
+                var thirdGet = epciser.TryGetPV(pvName);
+                if(firstGet.ToString() == secondGet.ToString() && secondGet.ToString() == thirdGet.ToString())
+                {
+                    return firstGet;
+                }
+            }
+            return "Nothing";
+        }
+
+        /// <summary>
         /// 对已经存在的PV设置值
         /// </summary>
         /// <param name="pvName"></param>
